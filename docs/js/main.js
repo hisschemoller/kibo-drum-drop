36f743e6177f12576ba263b820efc647a1383a0f
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2020-08-06 11:31:41 +0200

    Project setup with a copy of kibo-keyboard.

---
+++ docs/js/main.js
@@ -0,0 +1,28 @@
+import { dispatch, getActions, getState, persist } from './store/store.js';
+import { setup as setupAudio } from './audio/audio.js';
+import { accessMidi, setup as setupMidi, } from './midi/midi.js';
+import { setup as setupBluetooth } from './bluetooth/bluetooth.js';
+import { setup as setupControls } from './view/controls.js';
+import { setup as setupSettings } from './view/settings.js';
+
+async function main() {
+  let hasMIDIAccess = false;
+  try {
+    await accessMidi();
+    hasMIDIAccess = true;
+  } catch(error) {
+    console.log('Error', error);
+  } finally {
+    setupAudio();
+    setupSettings();
+    setupControls();
+    persist();
+    setupMidi();
+    setupBluetooth();
+    dispatch(getActions().setMidiAccessible(hasMIDIAccess));
+    dispatch(getActions().populate());
+    dispatch(getActions().toggleSettings(true));
+  }
+}
+
+main();
